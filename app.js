92fe6eb2865a4c9714693325e9034c644ac97de7
PatricioC55 <patriciochdz14@gmail.com>
2022-09-12 22:11:57 -0500

    Clase 4 terminado

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
-const btn = document.querySelector( '[data-form-btn]' );
+( ()=>{
+  const btn = document.querySelector( '[data-form-btn]' );
 
-const newTask = e => {
+  const newTask = e => {
 
-  e.preventDefault();
-  const input = document.querySelector( '[data-form-input]' );
-  const cardList = document.querySelector( '[data-list]' );
-  //cardList.appendChild( crearTarea( input.value ) );
+    e.preventDefault();
+    const input = document.querySelector( '[data-form-input]' );
+    const cardList = document.querySelector( '[data-list]' );
+    const divList = document.createElement( 'DIV' );
 
-  const contentCard = 
-    `<div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${input.value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    divList.appendChild( generateCheck() );
+    divList.appendChild( generateSpanTask( input.value ) );
 
-  const card = document.createElement( 'LI' );
-  card.classList.add( 'card' );
-  card.innerHTML = contentCard;
+    const card = document.createElement( 'LI' );
+    card.classList.add( 'card' );
+    card.appendChild( divList );
+    card.appendChild( generateTrashIcon() );
+    //card.innerHTML += contentCard;
 
-  cardList.appendChild( card );
+    cardList.appendChild( card );
 
-  input.value = '';
-} 
+    input.value = '';
+  } 
 
-btn.addEventListener('click', newTask );
+  btn.addEventListener('click', newTask );
 
+  const completeTask = e => {
+    const element = e.target;
+    superToggle( element, [ 'far', 'fas', 'completeIcon' ] );
+    //element.classList.toggle('far' );
+    //element.classList.toggle('fas');
+    //element.classList.toggle('completeIcon');
+  }
+
+  const deleteTask = e => {
+    const element = e.target;
+    //const beforeElement = element.parentElement();
+    
+  }
+
+  const generateCheck = () => {
+    //<i class="far fa-check-square icon"></i>
+    const check = document.createElement( 'I' );
+    check.classList.add( 'far', 'fa-check-square', 'icon' );
+    check.addEventListener( 'click', completeTask );
+    return check;
+  }
+
+  const generateSpanTask = ( textContent = '') => {
+    //<span class="task">${input.value}</span>
+    const spanTask = document.createElement( 'SPAN' );
+    spanTask.classList.add( 'task' );
+    spanTask.textContent = textContent;
+    return spanTask;
+  }
+
+  const generateTrashIcon = () => {
+    //<i class="fas fa-trash-alt trashIcon icon"></i>
+    const trasIcon = document.createElement( 'I' );
+    trasIcon.classList.add( 'fas', 'fa-trash-alt', 'trashIcon', 'icon' );
+    trasIcon.addEventListener( 'click', deleteTask );
+    return trasIcon;
+  }
+
+  function superToggle( object, classes = [] ){
+    if ( classes.isArray() ) {
+
+      classes.forEach( classItem => {
+        object.classList.toggle( classItem );
+      });
+
+    } else {
+      console.error("Invalid parameters");
+    }
+  }
+
+} )();
